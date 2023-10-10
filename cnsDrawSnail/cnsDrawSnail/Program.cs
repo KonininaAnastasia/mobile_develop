2a5abf9cd6fdef2614a838eb51abdec92682b066
KonininaAnastasia <kad28jun2003@gmail.com>
2023-10-10 14:42:04 +0300

    Spiral doesn't work (d/z)

---
+++ cnsDrawSnail/cnsDrawSnail/Program.cs
@@ -0,0 +1,112 @@
+﻿
+
+bool isContinue;
+bool isPaint;
+
+do
+{
+    Console.WriteLine("Длина?");
+    int.TryParse(Console.ReadLine(), out int length);
+
+    Console.WriteLine("Ширина? (Нечетное число)");
+    int.TryParse(Console.ReadLine(), out int wigth);
+
+    Console.WriteLine("Символ?");
+    string? simbol = Console.ReadLine();
+
+    char s = Convert.ToChar(simbol);
+
+    Console.WriteLine("Направление? [>/<] ->");
+
+    string space1 = "";
+
+    for (int j = 0; j < length - 1; j++)
+    {
+        space1 += " ";
+    }
+
+    string space2 = "";
+    for (int j = 1; j < length - 1; j++)
+    {
+        space2 += " ";
+    }
+
+    string space3 = " ";
+
+    if (isPaint = Console.ReadLine() == ">")
+    {
+        Console.WriteLine(new String(s, length));
+        Console.WriteLine(space1 + s);
+
+        for (int i = 2; i < wigth / 2 - 1; i++)
+        {
+
+            if (i % 2 == 0) //если змея ползет горисонтально в этой строке (верхняя половина)
+            {
+                if (i != 2)
+                    Console.Write(new String(' ', i));
+
+                for (int j = 0; j < length - Math.Pow(2, i - 1); j++) //длина
+                    Console.Write(s);
+                Console.WriteLine(space3 + s);
+                space3 += "  ";
+
+                Console.WriteLine(s + space2 + s); //ширина
+            }
+        }
+        //Console.WriteLine("закончился цикл");
+
+        if (wigth > 5)
+        {
+            Console.Write(s + " ");
+            Console.Write(new String(s, length - 6));
+            Console.Write(" " + s + " " + s);
+
+            Console.WriteLine();
+        }
+        else if (wigth <= 5 && wigth > 3)
+        {
+            Console.Write(new String(s, length - 2));
+            Console.Write(" " + s);
+            Console.WriteLine();
+        }
+
+        for (int i = wigth / 2 - 2; i >= 1; i--)
+        {
+            if (i % 2 == 1) //если змея ползет горисонтально в этой строкем (нижняя половина)
+            {
+                Console.WriteLine(space2 + s);
+
+                Console.Write(new String(' ', i));
+                for (int j = 0; j < length - Math.Pow(2, i); j++)
+                    Console.Write(s);
+                Console.WriteLine(space3 + s);
+                space3 += " ";
+
+            }
+
+        }
+        if (wigth > 3)
+        {
+            Console.Write(s);
+            Console.Write(new String(' ', length - 2));
+            Console.WriteLine(s);
+            Console.WriteLine(new String(s, length));
+        }
+        else
+            Console.WriteLine(new String(s, length));
+
+
+    }
+    else
+    {
+        for (int i = 0; i < wigth; i++)
+        {
+            Console.WriteLine(new String(s, length));
+        }
+    }
+
+    Console.WriteLine("Повторить? [Y/N] ->");
+    isContinue = Console.ReadLine()?.ToUpper() == "Y";
+
+} while (isContinue);
---
+++ cnsGenDice/cnsGenDice/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Количество кубиков (от 1 до 10)");
+int count_cubes = 0;
+while (count_cubes < 1 || count_cubes > 10) { int.TryParse(Console.ReadLine(), out count_cubes); }
+
+
+Console.WriteLine("Количество граней у кубика (не меньше 1; умолчанию 6)");
+int count_edge;
+int.TryParse(Console.ReadLine(), out count_edge);
+if (count_edge < 1) { count_edge = 6; }
+
+
+Console.WriteLine("Значения на гранях кубика (не меньше 1; по умолчанию(1, 2, 3, ...)");
+
+List<int> values = new List<int>();
+
+for (int i = 0; i < count_edge; i++)
+{
+    Console.WriteLine($"Введите значение грани {i + 1}");
+    int.TryParse(Console.ReadLine(), out int value);
+    if (value < 1)
+    {
+        value = i + 1;
+        Console.WriteLine(value);
+    }
+    values.Add(value);
+}
+for (int i = 0; i < count_cubes; i++)
+{
+    Random random = new Random();
+    int randomIndex = random.Next(0, values.Count);
+    Console.Write(values[randomIndex]);
+    Console.Write(" ");
+}
