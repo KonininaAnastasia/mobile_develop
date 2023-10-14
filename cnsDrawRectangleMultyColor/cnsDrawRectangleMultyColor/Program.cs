73e32e222ca1d23ca98ce6335800ba5152b1e412
KonininaAnastasia <kad28jun2003@gmail.com>
2023-10-14 23:35:37 +0300

    some nonsense

--- cnsDrawRectangleMultyColor/cnsDrawRectangleMultyColor/Program.cs
+++ cnsDrawRectangleMultyColor/cnsDrawRectangleMultyColor/Program.cs
@@ -15,13 +15,8 @@ do
     Console.WriteLine("Количество цветов?");
     int.TryParse(Console.ReadLine(), out int color_int);
 
-/*    ConsoleColor[] colors = { ConsoleColor.Black, ConsoleColor.DarkBlue, ConsoleColor.DarkGreen, ConsoleColor.DarkCyan,
-    ConsoleColor.DarkRed, ConsoleColor.DarkMagenta, ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.DarkGray,
-    ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Red, ConsoleColor.Magenta, ConsoleColor.Yellow,
-    ConsoleColor.White};*/
-
-    List<int> colors = new List<int>();
-
+    //List<string> colors = new List<string>();
+    string[] colors = new string[color_int];
 
     Console.WriteLine();
     Console.WriteLine("Цвета:");
@@ -32,86 +27,97 @@ do
 
     Console.WriteLine();
 
-
     for (int i = 0; i < color_int; i++)
     {
         Console.WriteLine($"Введите цвет {i+1}");
-        string c = Console.ReadLine();
-        int.TryParse(c, out int color);
-        colors.Add(color);
+        colors[i] = Console.ReadLine();
+        //string c = Console.ReadLine();
+        //colors.Add(c);
     }
+
+    Console.WriteLine(String.Join(' ', colors));
 
     Console.WriteLine("Символ?");
     string? simbol = Console.ReadLine();
 
     char s = Convert.ToChar(simbol);
 
-    string space = "";
+    int max_ount_color = width <= length ? width / 2 : length / 2; // для повтора цветов
+
+    ConsoleColor color;
 
     if (length > 0 && width > 0)
     {
         if (width / 2 <= length / 2)
         {
+            int colorIndex = 0;
             int l = length;
             for (int j = 0; j < width / 2; j++)
             {
-                Console.Write(space);
-                Console.WriteLine(new String(s, l));
+
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[colorIndex]);
+
+                Console.Write(new String(s, j));
+
+                if (j != 0)
+                    colorIndex = (colorIndex + 1) % colors.Length;
+                //Console.ResetColor();
+
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[colorIndex]);
+
+                Console.Write(new String(s, l));
+
+                //Console.ResetColor();
+                if (j != 0 && colorIndex != 0)
+                    colorIndex = (colorIndex - 1) % colors.Length;
+
+                if (colorIndex == 0)
+                    colorIndex = 2;
+
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colors[colorIndex]);
+
+                //Console.ResetColor();
+
+                Console.WriteLine(new String(s, j));
+
                 if (l > 1)
                     l -= 2;
-                space += " ";
+
+                if (j != 0)
+                    colorIndex = (colorIndex + 1) % colors.Length;
+
             }
-            space = "";
-            for (int j = 0; j < width / 2; j++)
+            for (int j = width / 2; j >= 0; j--)
             {
-                space += " ";
-            }
-            for (int j = width / 2; j > 0; j++)
-            {
-                Console.Write(space);
-                Console.WriteLine(new String(s, l));
+
+                //if (j < color_int)
+                //{
+                //    Enum.TryParse(colors[j], true, out color);
+                //    Console.ForegroundColor = color;
+                //}
+
+                Console.Write(new String(s, j));
+                Console.Write(new String(s, l));
+                Console.WriteLine(new String(s, j));
+
                 l += 2;
-                space.TrimEnd(' ');
+
                 if (l == length) { break; }
+
+                Console.ResetColor();
+
             }
-            
+
         }
         if (width / 2 > length / 2)
         {
 
         }
 
-        /*        for (int j = 0; j < length - 2; j++)
-                {
-                    space += " ";
-                }
-
-                if (width > 1)
-                {
-                    Console.WriteLine(new String(s, length));
-                    for (int i = 0; i < width - 2; i++)
-                    {
-                        if (length > 1)
-                        {
-                            Console.WriteLine(s + space + s);
-                        }
-                        else
-                        {
-                            Console.WriteLine(s);
-                        }
-                    }
-                    Console.WriteLine(new String(s, length));
-                }
-                else
-                {
-                    Console.WriteLine(new String(s, length));
-                }*/
     }
+    Console.ResetColor();
 
-    
-
-
-        Console.WriteLine("Повторить? [Y/N] ->");
+    Console.WriteLine("Повторить? [Y/N] ->");
     isContinue = Console.ReadLine()?.ToUpper() == "Y";
 
 } while (isContinue);
